efd793b075a8b12c544c8ef0ea8892f21a974672
Sal Ferrarello <sal@ironcodestudio.com>
2017-08-21 22:30:59 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# WordPress Post Meta Query Builder
+
+WordPress defines the [wp_postmeta database table](https://codex.wordpress.org/Database_Description#Table:_wp_postmeta) in a way that is very powerful but also challenging to query at times.
+
+Specifically, creating a database query that returns the post meta information with keys as columns and each row sharing the same post_id.
+
+This project helps me generate these queries quickly.
+
+Built with [Vue.js](https://vuejs.org/) and [Bootstrap](https://getbootstrap.com/).
+
+## Credits
+
+Sal Ferrarello ([salcode](https://github.com/salcode/))
---
+++ js/app.js
@@ -0,0 +1,71 @@
+function Row() {
+	this.key = '';
+	this.comparison  = 'none';
+	this.value = '';
+}
+var app = new Vue({
+	el: '#app',
+	data: {
+		rows: [],
+		resultingQuery: ''
+	},
+	methods: {
+		addRow: function() {
+			this.rows.push( new Row() );
+		},
+		removeRow: function( row ) {
+			this.rows.splice(this.rows.indexOf(row), 1)
+			this.queryCalc();
+		},
+		calcVol: function( row ) {
+			row.volume = row.length * row.width * row.height;
+			this.updateTotalVol();
+		},
+		updateQuery: function() {
+			this.resultingQuery = this.queryCalc();
+		},
+		queryCalc: function() {
+			var query =  '';
+			var index;
+
+			query += "SELECT\n";
+			this.rows.forEach( function ( row, index ) {
+				if ( 0 !== index ) {
+					query += ",\n";
+				}
+				query += "\tm" + index + ".meta_value AS " + row.key;
+			});
+
+			query += "\n";
+			query += "FROM wp_postmeta m0";
+
+			this.rows.forEach( function( row, index ) {
+				// Skip the first entry.
+				if ( 0 === index ) {
+					return true;
+				}
+				query += "\nLEFT JOIN wp_postmeta m" + index + "\n";
+				query += "\tON m0.post_id = m" + index + ".post_id AND m" + index + ".meta_key = '" + row.key + "'";
+			});
+
+			query += "\n";
+			query += "WHERE m0.meta_key = '" + this.rows[0].key + "'";
+
+			this.rows.forEach( function( row, index ) {
+				if ( 'none' === row.comparison ) {
+					// Skip this iteration.
+					return true;
+				}
+				query += "\n";
+				query += "\tAND m" + index + ".`meta_value` " + row.comparison + " '" + row.value +
+					"' -- " + row.key;
+			});
+
+			query += ";";
+
+
+
+			return query;
+		}
+	}
+});
